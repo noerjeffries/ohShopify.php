c8205474919c3accb8678ab25616a07be04efa70
Tomáš Gajdošík <gajdosiktomas@gmail.com>
2015-01-30 21:20:20 +0100

    curl follow redirects workaround when open_basedir and safe_mode are off

--- shopify.php
+++ shopify.php
@@ -99,7 +99,7 @@ class ShopifyClient {
 		$url = $this->curlAppendQuery($url, $query);
 		$ch = curl_init($url);
 		$this->curlSetopts($ch, $method, $payload, $request_headers);
-		$response = curl_exec($ch);
+		$response = $this->curlExecFollow($ch, 3);
 		$errno = curl_errno($ch);
 		$error = curl_error($ch);
 		curl_close($ch);
@@ -153,6 +153,74 @@ class ShopifyClient {
 		}
 
 		return $headers;
+	}
+
+	private function curlExecFollow($ch, $maxredirect = null)
+	{
+		$mr = $maxredirect === null ? 5 : intval($maxredirect);
+
+		if (ini_get('open_basedir') == '' && in_array(strtolower(ini_get('safe_mode')), array('off', '', '0')))
+		{
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+			curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		} else
+		{
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+			if ($mr > 0)
+			{
+				$original_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+				$newurl = $original_url;
+
+				$rch = curl_copy_handle($ch);
+
+				curl_setopt($rch, CURLOPT_HEADER, true);
+				curl_setopt($rch, CURLOPT_NOBODY, true);
+				curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+				do {
+					curl_setopt($rch, CURLOPT_URL, $newurl);
+					$header = curl_exec($rch);
+					if (curl_errno($rch))
+					{
+						$code = 0;
+					} else
+					{
+						$code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+						if ($code == 301 || $code == 302)
+						{
+							preg_match('/Location:(.*?)\n/i', $header, $matches);
+							$newurl = trim(array_pop($matches));
+
+							// if no scheme is present then the new url is a
+							// relative path and thus needs some extra care
+							if (!preg_match("/^https?:/i", $newurl))
+							{
+								$newurl = $original_url . $newurl;
+							}
+						} else
+						{
+							$code = 0;
+						}
+					}
+				} while ($code && --$mr);
+
+				curl_close($rch);
+
+				if (!$mr)
+				{
+					if ($maxredirect === null)
+						trigger_error('Too many redirects.', E_USER_WARNING);
+					else
+						$maxredirect = 0;
+
+					return false;
+				}
+				curl_setopt($ch, CURLOPT_URL, $newurl);
+			}
+		}
+		return curl_exec($ch);
 	}
 	
 	private function shopApiCallLimitParam($index)
